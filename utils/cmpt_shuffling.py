240d65fd54c5bd5d9d189da8cbb256ca76eee441
NevVerVer <kuzmina7748@gmail.com>
2023-09-16 22:10:14 +0300

    added structure of files

---
+++ utils/cmpt_shuffling.py
@@ -0,0 +1,9 @@
+"""
+Created on Sat September 16 21:15:06 2023
+Description: Controller class implementations for:
+TODO: paper url here
+@author: Ekaterina Kuzmina, ekaterina.kuzmina@skoltech.ru
+@author: Dmitrii Kriukov, dmitrii.kriukov@skoltech.ru
+"""
+
+# TODO: Dima
